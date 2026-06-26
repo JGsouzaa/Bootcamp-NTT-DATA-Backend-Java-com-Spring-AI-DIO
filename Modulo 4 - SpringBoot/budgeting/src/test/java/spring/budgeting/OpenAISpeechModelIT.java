855c32c212055601872e5ca7d966e4523f9b97df
jgabriel <jgabriel.souza11@gmail.com>
2026-06-26 16:45:21 -0300

    Speech API

---
+++ Modulo 4 - SpringBoot/budgeting/src/test/java/spring/budgeting/OpenAISpeechModelIT.java
@@ -0,0 +1,34 @@
+package spring.budgeting;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@EnabledIfEnvironmentVariable(named = "OPENAI_API_KEY", matches = ".+")
+public class OpenAISpeechModelIT {
+    @Autowired
+    OpenAiAudioSpeechModel openAiSpeechModel;
+
+    @Test
+    public void should_produceAudio_when_textIsProvided() throws IOException {
+        var response = openAiSpeechModel.call("O valor total do serviço ficou em 80 reais. Posso confirmar o pagamento?");
+
+        assertThat(response).hasSizeGreaterThan(1024);
+
+        var tempFile = Files.createTempFile("AUDIO_", ".mp3");
+        Files.write(tempFile, response);
+        System.out.println(tempFile.toAbsolutePath());
+    }
+}
